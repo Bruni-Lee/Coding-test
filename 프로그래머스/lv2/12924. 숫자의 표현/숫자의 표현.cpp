542e4b34c3fd94f317637fbaf70a487fcd640c64
HanSung Lee <dlgkstjd0534@naver.com>
2023-09-19 14:21:39 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0,sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j <= n; j++)
+        {
+            sum += j;
+            if (sum == n)
+            {
+                answer++;
+                break;
+            }                
+            else if (sum > n)
+                break;
+        }
+        sum = 0;
+    }
+    return answer;
+}
